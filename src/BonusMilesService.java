b4b54506904c267224d6455e11602151b537ad2d
Станислав Койфман <stanislavkoyfman@gmail.com>
2024-02-25 16:17:40 +0300

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price) {
-        int oneBonusAmount = 20;
-        return price / oneBonusAmount;
+
+    // Индекс = масса (кг)/ рост(м2) = (масса (кг) * 10_000) / рост(см2)
+    public int calculate(int weightKilo, int heightСentimeters) {
+        return (weightKilo * 10_000) / (heightСentimeters * heightСentimeters);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price); // должно получиться 500
-        System.out.println("Количество бонусных миль - " + miles);
+        int weightKilo = 98;
+        int heightСentimeters = 187;
+        int bmiIndex = service.calculate(weightKilo, heightСentimeters);
+        System.out.println("Индекс BMI - " + bmiIndex);
     }
 }
